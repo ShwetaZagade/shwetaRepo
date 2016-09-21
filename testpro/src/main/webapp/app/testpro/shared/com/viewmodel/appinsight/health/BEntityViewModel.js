788875ecec788bfeec01e09801cea2c057afea6e
deepali.arvind@algorhythm.co.in <applifire@6d50a702fb1e>
2016-09-21 09:15:25 +0000

    File deleting

--- testpro/src/main/webapp/app/testpro/shared/com/viewmodel/appinsight/health/BEntityViewModel.js
+++
@@ -1,6 +0,0 @@
-Ext.define('Testpro.testpro.shared.com.viewmodel.appinsight.health.BEntityViewModel', {
-     "extend": "Ext.app.ViewModel",
-     "alias": "viewmodel.BEntityViewModel",
-     "model": "BEntityModel",
-     "data": {}
-});
