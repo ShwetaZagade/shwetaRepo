9910ce164486e689f746b9603e816f40b915263a
deepali.arvind@algorhythm.co.in <applifire@6d50a702fb1e>
2016-09-21 09:04:25 +0000

    File deleting

--- testpro/src/main/webapp/app/testpro/shared/com/viewmodel/appinsight/health/CEntityViewModel.js
+++
@@ -1,6 +0,0 @@
-Ext.define('Testpro.testpro.shared.com.viewmodel.appinsight.health.CEntityViewModel', {
-     "extend": "Ext.app.ViewModel",
-     "alias": "viewmodel.CEntityViewModel",
-     "model": "CEntityModel",
-     "data": {}
-});
